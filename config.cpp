d7a7202c32f8c09bb30b18307b41d6113d6d2f32
Ben van Loon <Bennie_vloon@outlook.com>
2023-10-28 16:43:01 +0200

    Added CBA's version check

--- config.cpp
+++ config.cpp
@@ -1,3 +1,5 @@
+#include "script_version.hpp"
+
 class cfgPatches
 {
 	class lt_template_gear
@@ -5,10 +7,34 @@ class cfgPatches
 		name = "lt_template_gear_vn";
 		author = "Lowlands Tactical";
 		url = "http://lowtac.nl/";
-		requiredVersion = 1.0;
+		requiredVersion = 2.14;
 		requiredAddons[] = {"lt_template_base"};
 		units[] = {};
 		weapons[] = {};
+
+		//CBA versioning
+		version = VERSION;
+		versionStr = STR(VERSION_STR);
+		versionAR[] = {VERSION_AR};
+	};
+};
+
+class CfgSettings 
+{
+	class CBA 
+	{
+		class Versioning 
+		{
+			class LT_Gear
+			{
+				main_addon = "LT_Template_Gear";
+				class Dependancies 
+				{
+					LT_Base[] = {"LT_Template_Base", {3,23,156}, "true"};
+					LT_Gear_VN[] = {"LT_Template_Gear_VN", {VERSION_AR}, "true"};
+				};
+			};
+		};
 	};
 };
 
